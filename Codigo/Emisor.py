cffb0f96b927c1975bf76b35043c122f1dab6df5
jaquiroz <jorge.armando.quiroz.perez@gmail.com>
2020-11-16 02:20:03 -0400

    Incluido codificacion Manchester Diferencial

--- Codigo/Emisor.py
+++ Codigo/Emisor.py
@@ -3,16 +3,7 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-bits=[5,17,18,22,23,24,25,6]
-GPIO.setup(bits[0],GPIO.OUT)
-GPIO.setup(bits[1],GPIO.OUT)
-GPIO.setup(bits[2],GPIO.OUT)
-GPIO.setup(bits[3],GPIO.OUT)
-GPIO.setup(bits[4],GPIO.OUT)
-GPIO.setup(bits[5],GPIO.OUT)
-GPIO.setup(bits[6],GPIO.OUT)
-GPIO.setup(bits[7],GPIO.OUT)
-
+GPIO.setup(5,GPIO.OUT)
 DHT_Sensor = Adafruit_DHT.DHT11
 DHT_Pin = 4
 
@@ -37,23 +28,53 @@ try:
                     num_bin_inv += '1'
             temp_bin = num_bin_inv[::-1]
             
-            #print(temp_bin)
+            #Codificacion Manchester Diferencial
             
-            #Activacion de un led representando al numero binario transformado
             n=len(temp_bin)
-            num_bits=(8-n)*'0'+temp_bin
-            num_bin_list = list(num_bits)
-            m=len(num_bin_list)
-            i=0
-            for i in range (m-1):
-                if num_bin_list[i] == '1':
+            temp_bin=(10-n)*'0'+temp_bin
+            temp_bin_inv= temp_bin[::-1]
+            temp_bin_inv_list = list(temp_bin_inv)
+            n_temp_bin_inv_list =len(temp_bin_inv_list)
+            man_diff=n_temp_bin_inv_list*['']
+            dat='01'
+
+            if temp_bin_inv_list[1] == '0': #Primera condicion de Manchester Diferencial
+                man_diff[0]=dat[::-1]
+            elif temp_bin_inv_list[1] == '1':
+                man_diff[0]=dat
+
+            i=1
+
+            while i<= n_temp_bin_inv_list - 1: #Sigue la regla de Manchester Diferencial para los siguientes terminos
+                if temp_bin_inv_list[i] =='0':
+                    man_diff[i]=man_diff[i-1]
+    
+                elif temp_bin_inv_list[i] == '1':
+                    aux=man_diff[i-1]
+                    man_diff[i]=aux[::-1]
+        
+                i+=1
+
+            man_diff_str=''
+
+            for i in range(n_temp_bin_inv_list):
+                man_diff_str+=str(man_diff[i])
+    
+            man_diff_str='0'+man_diff_str+'1'
+            man_diff_list=list(man_diff_str)
+    
+            print(temp_bin)
+            print(man_diff_list)
+            
+            for i in man_diff_list:
+                if i == '1':
                     #print ("Es uno")
-                    GPIO.output(bits[i], GPIO.HIGH)
+                    GPIO.output(5, GPIO.HIGH)
+                    time.sleep(1)
                 else:
                     #print ("Es cero").
-                    GPIO.output(bits[i], GPIO.LOW)
-            
-            
+                    GPIO.output(5, GPIO.LOW)
+                    time.sleep(1)
             
         else:
             print("Falla en la lectura.")
